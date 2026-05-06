22ad3bdaf729f81e99c8d66aa1c50bfa6aeb6114
Chelsea Balmer <balmerct@gmail.com>
2026-05-06 11:17:34 +0200

    added database (mongoose) functionality to game thoughts tab

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import waypointRouter from './waypoints';
+import gameThoughtRouter from './gamethoughts';
 
 const router = Router();
 
 router.use('/waypoints', waypointRouter);
+router.use('/gamethoughts', gameThoughtRouter);
 
 export default router;
